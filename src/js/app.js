a9952e9d4e4eeb10ce1e3294f5331067ea234760
Nika Pavlenko <nikapavlenko20.np@gmail.com>
2020-09-05 22:34:28 +0300

    add all scss, swiper,html in block

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+const mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
